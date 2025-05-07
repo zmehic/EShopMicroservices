b31f3184b4d730909976294c0491f33f8b11017c
Zaim Mehic <zaim.mehic_7@hotmail.com>
2025-05-07 16:18:55 +0200

    gRPC implementation and consumation

---
+++ src/Services/Discount/DiscountgRPC/Data/DiscountContext.cs
@@ -0,0 +1,23 @@
+﻿using DiscountgRPC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiscountgRPC.Data
+{
+    public class DiscountContext:DbContext
+    {
+        public DbSet<Coupon> Coupons { get; set; } = default!;
+
+        public DiscountContext(DbContextOptions<DiscountContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Coupon>().HasData(
+                new Coupon { Id=1, ProductName="IPhone X", Description = "IPhone Discount", Amount = 150},
+                new Coupon { Id=2, ProductName="Samsung 10", Description="Samsung Discount", Amount = 100}
+                );
+        }
+    }
+}
